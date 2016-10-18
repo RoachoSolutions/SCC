1e8fa6b27d98f093586baacd10aaaee086f3e194
angel roacho <shadowdevelop@hotmail.com>
2016-10-18 17:56:11 -0500

    nuevo servicio

---
+++ SCC.Comun.Contantes/Enumeradores.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCC.Comun.Contantes
+{
+    public enum Couries{
+        RedPack=1,
+        DHL=2,
+        UPS=3,
+        FEDEX=4
+    }
+
+    public enum Acciones
+    {
+        Cotizacion=1,
+        CPCiudades=2,
+        Rastreo=3,
+        Recolecciones=4,
+        ImprecionGuia=5,
+        Tiempotrasito=6,
+        Cobertura=7,
+        ImpresionEtiqueta=8,
+        Retornos=9,
+        PapeleoImportacion=10,
+        PreparacioEnvios=11,
+        RedirigirEnvios=12
+    }
+}
---
+++ SCC.Couries.RedPack/clsInit.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCC.Couries.RedPack
+{
+    public class clsInit
+    {
+
+
+
+        public clsInit()
+        {
+            SCC.DAL.Config.couries _redpack = new DAL.Config.couries();
+            using (var context = new SCC.DAL.Config.SCC_ConfigEntities())
+            {
+                _redpack = context.couries.Where(x => x.idcouries == (int)SCC.Comun.Contantes.Couries.RedPack).FirstOrDefault();
+            }
+
+            if (_redpack.estado > 0)
+                throw new Exception("Couries no Activo");
+
+            
+
+        }
+
+        public void Cotizaciones()
+        {
+
+        }
+
+    }
+}
